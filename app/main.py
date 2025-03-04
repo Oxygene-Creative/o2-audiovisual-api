7d5dad6c526e31c1aff74f34e6f8c5358368a7bd
Brian Wachanga <brian@klurdy.com>
2025-03-04 21:33:09 +0300

    fix: include individual routers

--- app/main.py
+++ app/main.py
@@ -5,6 +5,10 @@ from dotenv import load_dotenv
 from contextlib import asynccontextmanager
 from app.core.redis import redis_router, redis_broker
 import os
+from app.pipelines.audio_analytics import audio_router
+from app.pipelines.reporting import reporting_router
+from app.pipelines.video_analytics import video_router
+from app.pipelines.transcript_analysis import transcript_router
 
 load_dotenv()
 
@@ -19,7 +23,10 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 # include faststream handlers
-app.include_router(redis_router)
+app.include_router(audio_router)
+app.include_router(video_router)
+app.include_router(transcript_router)
+app.include_router(reporting_router)
 
 # Include routers for modular endpoints
 app.include_router(embeddings_router, prefix="/embeddings", tags=["embeddings"])
