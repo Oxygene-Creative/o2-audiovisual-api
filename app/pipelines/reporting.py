d5343b89df993f71f15366ac55cc2c1ce3f2963b
Brian Wachanga <brian@klurdy.com>
2025-03-04 19:35:01 +0300

    update reporting broker

--- app/pipelines/reporting.py
+++ app/pipelines/reporting.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from faststream.redis import RedisRouter
+from faststream.redis import fastapi
 import os
 from app.core.redis import redis_broker
 
-reporting_router = RedisRouter(redis_broker)
+REDIS_URI = os.getenv("REDIS_URI", "redis://redis:6379")
+reporting_router = fastapi.RedisRouter(REDIS_URI)
 
 @reporting_router.subscriber("av_reporting")
 async def reporting_handler():
