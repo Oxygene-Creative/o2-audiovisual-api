3b818a60eff0595e5b97e7319e969cc23c3864a6
mcwachanga <b.wachanga@gmail.com>
2025-09-05 13:04:42 +0000

    update: fix queue locked bug

--- app/streams/queue.py
+++ app/streams/queue.py
@@ -1,22 +1,22 @@
 import json
-from app.core.redis import redis_broker as queue_broker, queue_busy_lock, redis_client
+from app.core.redis import redis_broker as queue_broker, worker_1_busy_lock, worker_2_busy_lock, worker_3_busy_lock, redis_client
 import asyncio
 
 async def queue_processor():
      while True:
-        if not queue_busy_lock.locked():
+        if not worker_1_busy_lock.locked() or not worker_2_busy_lock.locked() or not worker_3_busy_lock.locked():
             # retrieve the next 10 items in the queue
             results = await redis_client.zpopmax("audiovisual:priority_queue", count=1)
             
-            for result in results:
-                data_json, score = result
-                result_json = json.loads(data_json)
-
-                # post to media analysis
-                await queue_broker.publish(
-                    result_json, 
-                    stream="audiovisual:segmentation_stream"
-                )
+            data_json, score = results[0]
+            result_json = json.loads(data_json)
+            
+            # post to media analysis
+            await queue_broker.publish(
+                result_json, 
+                stream="audiovisual:segmentation_stream"
+            )
+                
                     
         else:
             print("🔁 AudioVisual Queue Still busy...")
